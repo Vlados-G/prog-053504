3afb628e301456e8d3467988f3106097c37ee575
DolgihDmitriy <82816379+DolgihDmitriy@users.noreply.github.com>
2021-04-24 16:56:33 +0300

    first commit (#31)
    Co-authored-by: DolgihPavel <dolgihgod@mail.ru>

---
+++ DolgihDmitriy/laba_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+unsigned long long fact(unsigned long long n)
+{
+	unsigned long long res = 1;
+	while (n > 1)
+		res *= n--;
+	return res;
+}
+
+int main()
+{
+	unsigned long long n;
+	printf("Enter number: ");
+	scanf_s("%llu", &n);
+	unsigned long long res = 0;
+	for (unsigned long long i = 1; i <= n; i++)
+	{
+		unsigned long long temp = 1;
+		for (unsigned long long j = 1; j <= i; j++)
+		{
+			temp *= fact(j + i) / fact(i);
+		}
+		res += temp;
+	}
+	printf("%llu", res);
+	return 0;
+
+}
