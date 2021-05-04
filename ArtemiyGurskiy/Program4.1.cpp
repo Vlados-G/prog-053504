46dd745f20fffaeb56d4d1aa67d7fb9b59ce6dcd
DaftCod <79042755+DaftCod@users.noreply.github.com>
2021-05-04 13:51:55 +0300

    lab4 (#41)

---
+++ ArtemiyGurskiy/Program4.1.cpp
@@ -0,0 +1,256 @@
+﻿#include <iostream>
+#define BUFFER_SIZE 256
+using namespace std;
+
+void calculate_shrink(char* s, int index);
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int i = 0;
+    int str_len = 0;
+
+    bool check = false;
+    bool no_operation_to_calculacte = false;
+
+    char s[BUFFER_SIZE];
+     
+    printf_s("Введите арифметическое выражение, не разделяя числа и операции пробелами: ");
+
+    do
+    {
+        scanf_s("%s", s, BUFFER_SIZE);
+
+        do
+        {
+            i++;
+        } while (s[i] != '\0');
+
+        str_len = i;
+
+        for(i = 0; i < str_len; i++)
+        {
+            if (s[i] != '0' && s[i] != '1' && s[i] != '2' && s[i] != '3' && s[i] != '4' && s[i] != '5' && s[i] != '6' && s[i] != '7' && s[i] != '8' && s[i] != '9' && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '/' && s[i] != '\0')
+            {
+                printf_s("\nВы ввели запрещённые символы, пожалуйста, повторите ввод.\n");
+                break;
+            }
+
+        }
+
+        if ((s[0] != '0' && s[0] != '1' && s[0] != '2' && s[0] != '3' && s[0] != '4' && s[0] != '5' && s[0] != '6' && s[0] != '7' && s[0] != '8' && s[0] != '9') || (s[str_len - 1] != '0' && s[str_len - 1] != '1' && s[str_len - 1] != '2' && s[str_len - 1] != '3' && s[str_len - 1] != '4' && s[str_len - 1] != '5' && s[str_len - 1] != '6' && s[str_len - 1] != '7' && s[str_len - 1] != '8' && s[str_len - 1] != '9'))
+        {
+            printf_s("\nСтрока должна начинаться и заканчиваться числом, пожалуйста, повторите ввод.\n");
+        }
+
+        else
+        {
+            check = true;
+        }
+
+    } while (!check);
+
+    printf_s("%s", s);
+
+    while(!no_operation_to_calculacte)
+    {
+        for(i = 0; i < str_len; i++)
+        {
+            if(s[i] == '*' || s[i] == '/')
+            {
+                calculate_shrink(s, i);
+
+                int k = 0;
+                do
+                {
+                    k++;
+                } while (s[k] != '\0');
+                str_len = k;
+                i = 0;
+            }
+        }
+
+        printf_s("\n\n%s", s);
+
+        for (i = 0; i < str_len; i++)
+        {
+            if (s[i] == '+' || s[i] == '-')
+            {
+                calculate_shrink(s, i);
+
+                int k = 0;
+                do
+                {
+                    k++;
+                } while (s[k] != '\0');
+                str_len = k;
+                i = 0;
+            }
+        }
+
+        printf_s("\n\n%s", s);
+
+        no_operation_to_calculacte = true;
+    }
+}
+
+void calculate_shrink(char* s, int index)
+{
+    int i = index;
+
+    int i_index = 0;
+
+    do
+    {
+        i_index++;
+    } while (s[i_index] != '\0');
+
+    int str_len = i_index;
+
+    char s1[BUFFER_SIZE];
+    char s2[BUFFER_SIZE];
+    char s3[BUFFER_SIZE];
+    
+    char op1_char[10];
+    char op2_char[10];
+
+    int op1_in_e = index - 1;
+    int op1_in_b = op1_in_e;
+
+    int op2_in_b = index + 1;
+    int op2_in_e = op2_in_b;
+    int result = 0;
+
+
+
+    while (s[op1_in_b] != '+' && s[op1_in_b] != '-' && s[op1_in_b] != '*' && s[op1_in_b] != '/' && op1_in_b != 0)
+    {
+        if(s[op1_in_b - 1] == '+' || s[op1_in_b - 1] == '-' || s[op1_in_b - 1] == '*' || s[op1_in_b - 1] == '/' || s[op1_in_b - 1] == 0)
+        {
+            break;
+        }
+        op1_in_b--;
+    }
+
+    int j;
+
+    for(j = 0; j <= op1_in_e-op1_in_b; j++)
+    {
+        op1_char[j] = s[(op1_in_b) + j];
+        
+    }
+    op1_char[j] = '\0';
+
+    int op1 = atoi(op1_char);
+
+
+
+
+    while (s[op2_in_e] != '+' && s[op2_in_e] != '-' && s[op2_in_e] != '*' && s[op2_in_e] != '/' && s[op2_in_e] != '\0')
+    {
+        if (s[op2_in_e + 1] == '+' || s[op2_in_e + 1] == '-' || s[op2_in_e + 1] == '*' || s[op2_in_e + 1] == '/' || s[op2_in_e + 1] == 0)
+        {
+            break;
+        }
+        op2_in_e ++;
+    }
+
+    int n;
+
+    for (n = 0; n <= op2_in_e - op2_in_b; n++)
+    {
+        op2_char[n] = s[op2_in_b + n];
+
+    }
+    op2_char[n] = '\0';
+
+    int op2 = atoi(op2_char);
+
+
+
+    if (s[index] == '*')
+    {
+        result = op1 * op2;
+    }
+
+    if (s[index] == '/')
+    {
+        result = op1 / op2;
+    }
+
+    if (s[index] == '+')
+    {
+        result = op1 + op2;
+    }
+
+    if (s[index] == '-')
+    {
+        result = op1 - op2;
+    }
+
+    _itoa_s(result, s2, _countof(s2), 10);
+
+
+
+    int k = 0;
+
+    for(k; k < op1_in_b; k++)
+    {
+        s1[k] = s[0 + k];
+    }
+
+    s1[k] = '\0';
+
+
+    int l = 0;
+
+    for (l; l < str_len - op2_in_e; l++)
+    {
+        if (s[op2_in_e + 1] == '\0')
+        {
+            break;
+        }
+        s3[l] = s[(op2_in_e + 1) + l];
+    }
+
+    s3[l] = '\0';
+
+
+
+
+    i_index = 0;
+    j = 0;
+    k = 0;
+    l = 0;
+
+
+    while(s1[k] != '\0')
+    {
+        s[i_index] = s1[k];
+
+        k++;
+        i_index++;
+    }
+    s[i_index] = '\0';
+
+
+    while (s2[j] != '\0')
+    {
+        s[i_index] = s2[j];
+
+        j++;
+        i_index++;
+    }
+    s[i_index] = '\0';
+
+
+    while (s3[l] != '\0')
+    {
+        s[i_index] = s3[l];
+
+        l++;
+        i_index++;
+    }
+    s[i_index] = '\0';
+}
---
+++ ArtemiyGurskiy/Program4.2.cpp
@@ -0,0 +1,154 @@
+﻿#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 128 /* количество символов в строке */
+
+int Strlen(const char* str) {
+	int len = 0;
+	while (*str)
+	{
+		str++;
+		len++;
+	}
+	return len;
+}
+
+void recognizer(char* text_buffer)
+{
+	int i_index = 0;
+	int j = 0;
+	int l = 0;
+	int text_buffer_length = Strlen(text_buffer);
+
+	char word_buffer1[N];
+	char space = ' ', enter = '\n', null = '\0';
+
+	while(true)
+	{
+		j = 0;
+
+		for (i_index; i_index <= text_buffer_length; i_index++)
+		{
+			if (text_buffer[i_index] != space && text_buffer[i_index] != enter)
+			{
+				if (text_buffer[i_index] != space && text_buffer[i_index] != enter && text_buffer[i_index] != null)
+				{
+					word_buffer1[j] = text_buffer[i_index];
+					text_buffer[i_index] = ' ';
+					word_buffer1[j + 1] = '\0';
+					j++;
+				}
+
+				if (text_buffer[i_index] == null)
+				{
+					exit(1);
+				}
+
+				if (text_buffer[i_index + 1] == ' ' || text_buffer[i_index + 1] == enter || text_buffer[i_index + 1] == null)
+				{
+					printf_s("\n%s ", word_buffer1);
+					break;
+
+				}
+			}
+		}
+
+		l = 0;
+		char word_buffer2[N];
+
+		for (i_index++; i_index <= text_buffer_length; i_index++, l++)
+		{
+			if (text_buffer[i_index] != space && text_buffer[i_index] != enter && text_buffer[i_index] != null)
+			{
+				word_buffer2[l] = text_buffer[i_index];
+				word_buffer2[l + 1] = '\0';
+			}
+			else
+			{
+				bool check = false;
+				for(int i = 0; i < j; i++)
+				{
+					check = false;
+
+					for(int j = 0; j < l; j++)
+					{
+						check = false;
+						if(word_buffer1[i] == word_buffer2[j])
+						{
+							check = true;
+							break;
+						}
+					}
+					if(check == false)
+					{
+						break;
+					}
+				}
+				for (int i = 0; i < j; i++)
+				{
+					check = false;
+
+					for (int j = 0; j < l; j++)
+					{
+						check = false;
+						if (word_buffer2[i] == word_buffer1[j])
+						{
+							check = true;
+							break;
+						}
+					}
+					if (check == false)
+					{
+						break;
+					}
+				}
+				if(check == true)
+				{
+					printf_s(" %s", word_buffer2);
+					for(int k = i_index - Strlen(word_buffer2); k < i_index; k++)
+					{
+						text_buffer[k] = ' ';
+					}
+				}
+				l = -1;
+			}
+		}
+
+		i_index = 0;
+	}
+}
+
+void main(void)
+{
+	system("chcp 1251");
+	system("cls");
+
+	int i_index = 0;
+
+	char temp_text_buffer[N];
+	char text_buffer[N];
+	FILE* fp;
+
+	fp = fopen("D:\\ex42b.txt", "r");
+
+	if (!fp)
+	{
+		exit(1);
+	}
+
+	while (fgets(temp_text_buffer, N, fp) != NULL)
+	{
+		puts(temp_text_buffer);
+		for(int i = 0; i < Strlen(temp_text_buffer); i++)
+		{
+			text_buffer[i_index] = temp_text_buffer[0 + i];
+			i_index++;
+		}
+		text_buffer[i_index] = ' ';
+	}
+	fclose(fp);
+
+	text_buffer[i_index] = '\0';
+	recognizer(text_buffer);
+}
