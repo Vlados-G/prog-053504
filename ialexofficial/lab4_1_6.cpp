ad202488cd945a0eeb3e22a87c81296ebad62284
ialexofficial <59487303+ialexofficial@users.noreply.github.com>
2021-05-15 16:50:28 +0300

    Исаеня Александр lab#4 variant-#6 (#28)
    * First commit
    
    * First commit
    
    * Upd
    
    * The last update
    
    * The last update 2
    
    * Last update
    
    * Delete lab2_1_17.c
    
    * Delete lab2_2_6.c

---
+++ ialexofficial/lab4_1_6.cpp
@@ -0,0 +1,85 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int strlen(char* str)
+{
+	int res = 0;
+	while (str[res] != '\0' && str[res] != '\n')
+		res++;
+	return res;
+}
+void concate(char* str1, char* str2)
+{
+	int pos = strlen(str1);
+	for (int i = 0; i < strlen(str2); i++)
+		str1[pos++] = str2[i];
+	str1[pos++] = ' ';
+	str1[pos] = '\0';
+}
+void clearStr(char* str)
+{
+	str[strlen(str)] = '1';
+	str[0] = '\0';
+}
+char* getResult(char* str)
+{
+	char prevChar;
+	char* temp = malloc(sizeof(char) * (strlen(str) + 2));
+	char* res = malloc(sizeof(char) * (strlen(str) + 2));
+	int tempPos = 0;
+	int chooseNext = 0;
+	res[0] = temp[0] = '\0';
+	prevChar = str[0];
+	for (int i = 1; i < strlen(str); i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (!chooseNext) {
+				temp[tempPos++] = prevChar;
+				temp[tempPos] = '\0';
+				concate(res, temp);
+			}
+			prevChar = str[i];
+			clearStr(temp);
+			tempPos = 0;
+			chooseNext = 0;
+		}
+		else if (str[i] < prevChar)
+		{
+			chooseNext = 1;
+			prevChar = ' ';
+			clearStr(temp);
+			tempPos = 0;
+		}
+		else {
+			if (prevChar != ' ') {
+				temp[tempPos++] = prevChar;
+				temp[tempPos] = '\0';
+			}
+			prevChar = str[i];
+		}
+	}
+	if (prevChar != ' ' && !chooseNext) {
+		temp[tempPos++] = prevChar;
+		temp[tempPos] = '\0';
+		concate(res, temp);
+	}
+	free(temp);
+	return res;
+}
+
+int main()
+{
+	int n;
+	printf("Size of string: ");
+	scanf(" %d", &n);
+	char* str = malloc(sizeof(char) * n);
+	printf("String: ");
+	fgets(str, n, stdin);
+	fgets(str, n, stdin);
+	char* res = getResult(str);
+	printf("Result :\n%s", res);
+	free(res);
+	free(str);
+	return 0;
+}
---
+++ ialexofficial/lab4_2_6.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int strlen(unsigned char* str)
+{
+	int res = 0;
+	while (str[res] != '\n' && str[res] != '\0')
+		res++;
+	return res;
+}
+int atoi(char c)
+{
+	return c - '0';
+}
+int pow(int base, int exp)
+{
+	int res = 1;
+	for (int i = 0; i < exp; i++)
+		res *= base;
+	return res;
+}
+int stoi(unsigned char* str)
+{
+	int res = 0, len = strlen(str);
+	for (int i = 0; i < len; i++)
+	{
+		int tmp = (int)pow(10, (len - 1));
+		res += atoi(str[i]) * pow(10, (len - 1));
+	}
+	return res;
+}
+void moveLetters(unsigned char* str, int n)
+{
+	n = n % ('z' - 'a');
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] += n;
+			if (str[i] % ('Z' + 1) < 'A')
+				str[i] = 'A' + (str[i] % 'Z') - 1;
+		}
+		else if (str[i] >= 'a' && str[i] <= 'z') {
+			str[i] += n;
+			if (str[i] % ('z' + 1) < 'a')
+				str[i] = 'a' + (str[i] % 'z') - 1;
+		}
+	}
+}
+
+int main()
+{
+	FILE* file = fopen("C:\\Users\\Asus\\Desktop\\input.txt", "r");
+	int n;
+	unsigned char str[100];
+	fgets(str, 100, file);
+	n = stoi(str);
+	for (int i = 0; i < n; i++)
+	{
+		fgets(str, 100, file);
+		str[strlen(str)] = '\0';
+		printf("%s - ", str);
+		moveLetters(str, i + 4);
+		printf("%s\n", str);
+	}
+	return 0;
+}
