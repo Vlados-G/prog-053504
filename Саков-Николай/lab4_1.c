5f39de7194b4300c09610dc51710e8c2b88bac67
sakovnv <65726867+sakovnv@users.noreply.github.com>
2021-05-15 14:32:51 +0300

    Саков Николай lab#4 variant-#17 (#79)
    * lab
    
    * added lab
    
    * deleted-folder
    
    * added-personal-folder
    
    * updated
    
    * added lab2
    
    * deleted lab1.c
     branch lab_2
    
    * Revert "deleted-folder"
    
    This reverts commit 0391d286ef3fdc84736a54590926ef3e4ea315e5.
    
    * returned-folder
    
    * recursion
    
    * added files

---
+++ Саков-Николай/lab4_1.c
@@ -0,0 +1,45 @@
+﻿#include <conio.h>
+#include <locale.h>
+#include <stdio.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char str[128], ch;
+	printf("Введите строку: \n");
+	int i = 0;
+	int brackets = 0;
+	while ((ch = getchar()) != '\n')
+	{
+		if (ch == '(')
+		{
+			brackets++;
+			str[i] = ch;
+			i++;
+			continue;
+		}
+		if (ch == ')')
+		{
+			brackets--;
+			str[i] = ch;
+			i++;
+			continue;
+		}
+		if (brackets > 0)
+		{
+			continue;
+		}
+		str[i] = ch;
+		i++;
+	}
+	str[i] = '\0';
+	printf("%s\n", str);
+	if (brackets > 0)
+	{
+		printf("У вас не закрыто %d скобок", brackets);
+	}
+	else if (brackets < 0)
+	{
+		printf("У вас не открыто %d скобок", brackets * -1);
+	}
+}
---
+++ Саков-Николай/lab4_2.c
@@ -0,0 +1,68 @@
+﻿/*4.2 (17).Латинизатор кириллицы.При интернет - общении с русской
+диаспорой в других странах часто возникает проблемы отсутствия
+кириллицы у зарубежных респондентов, а также слабое знание
+иностранных языков у соотечественников.Один из выходов – набор
+русских слов похожими по начертанию буквами латинского
+алфавита.Среди прописных русских букв таких насчитывается
+одиннадцать : А, В, Е, К, М, Н, О, Р, С, Т, Х.Распечатать из
+заданного файла те слова, которые без искажения могут быть
+написаны латинскими буквами, например :
+	ВОВКА РОЕТ РОВ
+	СВЕТА СЕЕТ ОВЕС.*/
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char letters[16] = "ABCEHKMOPTX";
+	char fletters[16] = "DFGIJLNQRSUVWYZ";
+	char buffer[128];
+	char word[16];
+	short incorrectWord = 0;
+	FILE *fp;
+	fp = fopen("words.txt", "r");
+	if (fp)
+	{
+		while (fgets(buffer, 128, fp))
+		{
+			int i = 0;
+			int wordsize = 0;
+			while (buffer[i] != '\0' && buffer[i] != '\n')
+			{
+				int k = 0; 
+				while (letters[k] != '\0')
+				{
+					if (buffer[i] == letters[k] && incorrectWord == 0)
+					{
+						word[wordsize++] = buffer[i];
+						break;
+					}
+					else if (buffer[i] == fletters[k])
+					{
+						wordsize = 0;
+						incorrectWord = 1;
+					}
+					k++;
+				}
+				i++;
+				if (buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '\0')
+				{
+					if (incorrectWord == 0)
+					{
+						word[wordsize] = '\0';
+						printf("%s ", word);
+						wordsize = 0;
+					}
+					else
+					{
+						incorrectWord = 0;
+					}
+				}
+			}
+		}
+	}
+}
