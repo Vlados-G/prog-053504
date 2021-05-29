ce93f2e9846a29e117401da2fc2171f463302e55
matuamod <83645297+matuamod@users.noreply.github.com>
2021-05-29 15:06:33 +0300

    Максимов Матвей lab#4 variant-#14 (#72)
    * Максимов Матвей lab#1 variant-#6
    
    * Максимов Матвей lab#2 variant-#10
    
    * Update Programming2laba1task.cpp
    
    * Максимов Матвей lab#2 variant-#6
    
    * Максимов Матвей lab#3 variant-#1
    
    * Максимов Матвей lab#4 variant-#14
    
    * Максимов Матвей lab#4 variant-#14
    
    * Delete Programming1stlaba.cpp
    
    * Delete Programming2laba1task.cpp
    
    * Delete Programming2laba2task.cpp
    
    * Delete Programming3laba.cpp

---
+++ MatveyMaximov/Programming4laba1task.cpp
@@ -0,0 +1,83 @@
+﻿// Programming 4 laba 1st task.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+void Part(int a) {
+    int i = 0;
+    while (i != a) {
+        printf("#");
+        i++;
+    }
+    printf("\n");
+}
+
+void Diagram(int a, int b, int c, int d, int e, int f, int s) {
+    float n1 = 100,n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0, n7 = 0;
+    printf("Diagram,which shows vowel dependence in text : \n\n\n");
+    printf("All word count in text is %d,",a);
+    printf("which is %f percents of the text. \n",n1);
+    Part(a);
+    n2 = (b * n1) / a;
+    printf("A/a word count in text is %d,", b);
+    printf("which is %f percents of the text. \n", n2);
+    Part(b);
+    n3 = (c * n1) / a;
+    printf("E/e word count in text is %d,", c);
+    printf("which is %f percents of the text. \n", n3);
+    Part(c);
+    n4 = (d * n1) / a;
+    printf("I/i word count in text is %d,", d);
+    printf("which is %f percents of the text. \n", n4);
+    Part(d);
+    n5 = (e * n1) / a;
+    printf("O/o word count in text is %d,", e);
+    printf("which is %f percents of the text. \n", n5);
+    Part(e);
+    n6 = (f * n1) / a;
+    printf("U/u word count in text is %d,", f);
+    printf("which is %f percents of the text. \n", n6);
+    Part(f);
+    n7 = (s * n1) / a;
+    printf("Y/u word count in text is %d,", s);
+    printf("which is %f percents of the text. \n", n7);
+    Part(s);
+}
+
+int main()
+{
+    int count_ALL = 0, count_A = 0, count_E = 0, count_I = 0, count_O = 0, count_U = 0, count_Y = 0;
+    char str[1000],ch;
+    int i = 0;
+    puts("Print your text : \n");
+    while ((ch = getchar()) != '!') {
+        count_ALL++;
+        if (ch == 'A' || ch == 'a') {
+            count_A++;
+        }
+        else if (ch == 'E' || ch == 'e') {
+            count_E++;
+        }
+        else if (ch == 'I' || ch == 'i') {
+            count_I++;
+        }
+        else if (ch == 'O' || ch == 'o') {
+            count_O++;
+        }
+        else if (ch == 'U' || ch == 'u') {
+            count_U++;
+        }
+        else if (ch == 'Y' || ch == 'y') {
+            count_Y++;
+        }
+        str[i++] = ch;
+    }
+    str[i] = '\0';
+
+    printf("\n\n");
+    Diagram(count_ALL, count_A, count_E, count_I, count_O, count_U, count_Y);
+    return 0;
+}
+
---
+++ MatveyMaximov/Programming4laba2task.cpp
@@ -0,0 +1,111 @@
+﻿// Programming 4 laba 2nd task.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <clocale>
+#include <ctime>
+#pragma warning(disable: 4996)
+#define N 100
+#define M 100
+
+int main()
+{
+    char arr[M][N];
+    char buffer[N];
+    int i = 0, j = 0, k = 0,count = 0;
+    FILE* fp;
+    fp = fopen("C:\\Users\\user\\source\\repos\\Programming4laba2ndtask\\Programming 4 laba 2nd task\\Debug\\FIO.txt", "r+");
+    if(fp == NULL){
+        printf("Cannot open file. \n\n");
+        exit(1);
+    }
+    else {
+        printf("File open. \n\n");
+    }
+    while (fgets(buffer, N, fp) != NULL) {
+        while(buffer[i] != '\0'){
+            if (i == 0) {
+                if ((int)buffer[i] >= 97 && (int)buffer[i] <= 122) {
+                        buffer[i] -= 32;
+                        printf("%c", buffer[i]);
+                }
+                else {
+                    printf("%c", buffer[i]);
+                }
+            }
+            else if (buffer[--i] == ' ') {
+                i++;
+                if ((int)buffer[i] >= 97 && (int)buffer[i] <= 122) {
+                    buffer[i] -= 32;
+                    printf("%c", buffer[i]);
+                }
+                else {
+                    printf("%c", buffer[i]);
+                }
+            }
+            else {
+                i++;
+                printf("%c", buffer[i]);
+            }
+            if (((int)buffer[i] >= 65 && (int)buffer[i] <= 90) || ((int)buffer[i] >= 97 && (int)buffer[i] <= 122) || (int)buffer[i] == 32 || buffer[i] == '\n') {
+                if (buffer[i] == '\n') {
+                    buffer[i] = '\0';
+                    arr[j][k] = buffer[i];
+                    k = 0;
+                }
+                else {
+                    arr[j][k] = buffer[i];
+                    k++;
+                }
+            }
+            i++;
+        }
+        i = 0;
+        j++;
+    }
+    for (int a = 0; a < j; a++) {
+        for (int b = 0; b < M; b++) {
+            if (((int)arr[a][b] >= 65 && (int)arr[a][b] <= 90) || ((int)arr[a][b] >= 97 && (int)arr[a][b] <= 122) || (int)arr[a][b] == 32 || arr[a][b] == '\n') {
+                if (count <= b) {
+                    count = b;
+                }
+            }
+            else {
+                break;
+            }
+        }
+    }
+    printf("\n\n\n\n\n");
+    printf("After sorting : \n");
+    for (int a = 0; a < j; a++) {
+        int buff = (int)arr[a][0];
+        for(int b = a + 1; b < j; b++){
+            if ((int)arr[b][0] < buff) {
+                buff = (int)arr[b][0];
+                int r = 0;
+                while (r < count + 3) {
+                    char buff = arr[a][r];
+                    arr[a][r] = arr[b][r];
+                    arr[b][r] = buff;
+                    r++;
+                }
+            }
+        }
+    }
+    for (int a = 0; a < j; a++) {
+        for (int b = 0; b < count + 3; b++) {
+            if (arr[a][b] == '\0') {
+                printf("\n");
+                b = 0;
+                break;
+            }
+            else {
+                printf("%c", arr[a][b]);
+            }
+        }
+    }
+    fclose(fp);
+}
