49fa20996c67f0e5c360b98b116dbdc13bc3ed5d
matuamod <83645297+matuamod@users.noreply.github.com>
2021-05-29 15:06:07 +0300

    Максимов Матвей lab#2 variant-#10 (#69)
    * Максимов Матвей lab#1 variant-#6
    
    * Максимов Матвей lab#2 variant-#10
    
    * Update Programming2laba1task.cpp
    
    * Максимов Матвей lab#2 variant-#6
    
    * Delete Programming1stlaba.cpp

---
+++ MatveyMaximov/Programming2laba1task.cpp
@@ -0,0 +1,86 @@
+﻿// Programming 2 laba 1 task.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+float CarEuropePrice, Distance,Capacity;
+int Year;
+
+float EnterCarPriceInEurope() {
+    printf("Car Price in Europe is (price in EURO) : ");
+    scanf_s("%f", &CarEuropePrice);
+    return CarEuropePrice;
+}
+int EnterTheYearOfTheIssue() {
+    printf("Year of the car manufacture is : ");
+    scanf_s("%d", &Year);
+    return Year;
+}
+float EnterEngineCapacity() {
+    printf("Capacity of the engine is (cm.cube) : ");
+    scanf_s("%f", &Capacity);
+    return Capacity;
+}
+float EnterDistanceOfTheRun() {
+    printf("Distance of the run is (kilometers) : ");
+    scanf_s("%f", &Distance);
+    return Distance;
+}
+float CalculateTheFinalPriceOfTheCar() {
+    float FinalDistancePrice;
+    float PriceOfOneKilometer = 0.5;
+    FinalDistancePrice = Distance * PriceOfOneKilometer;
+    printf("\nPrice of the distance haul is : %f EURO ", FinalDistancePrice);
+    float CarClearancePrice;
+    int CurrentYear = 2021;
+    if (Year >= CurrentYear - 3) {
+        CarClearancePrice = Capacity * 0.6;
+        printf("\nCar age is less than 3 years. \nPrice for the customs clearance is : %f EURO ", CarClearancePrice);
+    }
+    else if(Year < CurrentYear - 3 && Year >= CurrentYear - 10){
+        if (Capacity < 2500) {
+            CarClearancePrice = Capacity * 0.35;
+            printf("\nCar age is more than 3 years,but less than 10 years. Engine capacity is less than 2500 cm.cube.\n Price for the customs clearance is : %f EURO ", CarClearancePrice);
+        }
+        else {
+            CarClearancePrice = Capacity * 0.6;
+            printf("\nCar age is more than 3 years,but less than 10 years. Engine capacity is more than 2500 cm.cube. Price for the customs clearance is : %f EURO ", CarClearancePrice);
+        }
+    }
+    else if(Year < CurrentYear - 10 && Year >= CurrentYear - 14) {
+        CarClearancePrice = Capacity * 0.6;
+        printf("\nCar age is more than 10 years,but less than 14 years.\n Price for the customs clearance is : %f EURO ", CarClearancePrice);
+    }
+    else {
+        CarClearancePrice = Capacity * 2;
+        printf("\nCar age is more thsan 14 years.\n Price for the customs clearance is : %f EURO", CarClearancePrice);
+    }
+    float TheFinalPriceOfTheCar;
+    TheFinalPriceOfTheCar = CarEuropePrice + FinalDistancePrice + CarClearancePrice;
+    printf("\nThe final price of the car(including car age,engine capacity,price of the distance haul,price of the customs clearance) is : %f EURO", TheFinalPriceOfTheCar);
+    return CarClearancePrice;
+}
+void InformationOfTheCreator() {
+    printf("This calculation is actual for 2021 year.\nCreator of this car calculation is Matvey Maximov.");
+}
+
+int main()
+{
+    float CarEuropePrice, Distance;
+    int Year, Capacity,a;
+    EnterCarPriceInEurope();
+    EnterTheYearOfTheIssue();
+    EnterEngineCapacity();
+    EnterDistanceOfTheRun();
+    CalculateTheFinalPriceOfTheCar();
+    printf("\n\n\nIf you want to know the information of the program and creator,tab '1' : ");
+    scanf_s("%d", &a);
+    if (a == 1) {
+        InformationOfTheCreator();
+    }
+    return 0;
+}
+
---
+++ MatveyMaximov/Programming2laba2task.cpp
@@ -0,0 +1,88 @@
+﻿// Programming  2 laba 2 task.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+#define M_PI  3.14159265
+
+int n = 1;
+long double lol = 0;
+
+int factorial(int k){
+	int res = 0;
+	if (k < 0) {
+		return 0;
+	}
+	else if(k == 0) {
+		return 1;
+	}
+	else if(k >= 1) {
+		return  (k * factorial(k - 1));
+	}
+}
+
+ float rec(int n, long double x, long double E) {
+	
+	lol = pow((-1), (n - 1)) * pow((x * M_PI / 180), (2 * n - 1)) / factorial(2 * n - 1);
+	return lol;
+}
+
+ void buff(int n, long double x, long double E, long double left, long double right) {
+	 while (fabs(left - right) > E) {
+		 right += rec(n, x, E);
+		 n++;
+	 }
+	 while (fabs(left - right) < E) {
+		 printf("correct n is : %d \n", n);
+		 printf("Result of calculaton of the right side : \n");
+		 printf("right result = %lf \n", right);
+		 break;
+	 }
+}
+
+ void incr(int n, long double x, long double E, long double left, long double right) {
+	 while (n)
+	 {
+		 if (fabs(left - right) > E) {
+			 right += pow((-1), (n - 1)) * pow((x * M_PI / 180), (2 * n - 1)) / factorial(2 * n - 1);
+			 n++;
+		 }
+		 else {
+			 printf("correct n is : %d \n", n);
+			 printf("Result of calculaton of the right side : \n");
+			 printf("right result = %lf \n", right);
+			 break;
+		 }
+	 }
+ }
+
+int main()
+{
+	long double first_result = 0, second_result = 0,E,x;
+	int choice = 0;
+	printf("Enter minimum E : ");
+	scanf_s("%lf", &E);
+	printf("Enter angle : ");
+	scanf_s("%lf", &x);
+	while(x > 360) {
+		x -= 360;
+	}
+	while (x < -360) {
+		x += 360;
+	}
+	first_result = sin(x * M_PI / 180);
+	printf("Result of calculation of the left side : \n");
+	printf("left result = %lf \n", first_result);
+	printf("Enter 1 for recursion or 2 for incrementation : \n");
+	scanf_s("%d",&choice);
+	if (choice == 1) {
+		buff(n,x, E, first_result, second_result);
+	}
+	else if (choice == 2) {
+		incr(n, x, E, first_result, second_result);
+	}
+	return 0;
+} 
