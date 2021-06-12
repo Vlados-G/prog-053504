0b8dac83db526c45bf99523e8cf637811fa6c44a
Tim-Abozny <72353844+Tim-Abozny@users.noreply.github.com>
2021-06-12 17:51:24 +0300

    Обозный Артём lab#6 variant-#24 (#148)
    * First commit
    
    * second lab
    
    * Laba #3
    
    * Laba #4
    
    * Delete lab3(C).cpp
    
    * Delete task2.cpp
    
    * Add second task for 4 lab
    
    * Add 1 & 2 tasks to 4 lab
    
    * try to load
    
    * 5th lab
    
    * 6th lab
    
    * Delete lab5(Ci).c
    
    * Delete lab4_2(C).cpp
    
    * Delete lab4(C).cpp

---
+++ Tim-Abozny/lab6(Ci).c
@@ -0,0 +1,118 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <ctype.h>
+#include <malloc.h>
+#include <locale.h>
+#include <string.h>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct tr
+{
+	char value;
+	struct tr* left;
+	struct tr* right;
+};
+
+struct tr* toNode(char value, struct tr* tr)
+{
+	if (tr == NULL)
+	{
+		tr = malloc(sizeof(struct tr));
+		tr->value = value;
+		tr->left = NULL;
+		tr->right = NULL;
+	}
+	else if (toupper(value) < toupper(tr->value))
+	{
+		tr->left = toNode(value, tr->left);
+	}
+	else
+	{
+		tr->right = toNode(value, tr->right);
+	}
+	return tr;
+}
+
+void clTree(struct tr* tr)
+{
+	if (tr != NULL)
+	{
+		clTree(tr->left);
+		clTree(tr->right);
+		free(tr);
+	}
+}
+
+int ndCouter(struct tr* tr, char ch, short YesOrNot, int Counter)
+{
+	if (tr != NULL)
+	{
+		if (YesOrNot == 0)
+		{
+			if (tr->value == ch)
+			{
+				Counter = ndCouter(tr->left, ch, 1, Counter);
+				Counter = ndCouter(tr->right, ch, 1, Counter);
+			}
+			else
+			{
+				Counter = ndCouter(tr->left, ch, 0, Counter);
+				Counter = ndCouter(tr->right, ch, 0, Counter);
+			}
+		}
+		else
+		{
+			Counter++;
+			Counter = ndCouter(tr->left, ch, 1, Counter);
+			Counter = ndCouter(tr->right, ch, 1, Counter);
+		}
+	}
+	return Counter;
+}
+
+void Show(struct tr* tr, int n)
+{
+	if (tr->left)
+	{
+		Show(tr->left, n + 1);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		printf(" ");
+	}
+	printf("%c\n", tr->value);
+	if (tr->right)
+	{
+		Show(tr->right, n + 1);
+	}
+}
+
+int main()
+{
+	setlocale(0, "rus");
+	char str[128];
+	int howNodes;
+	struct tr* tr = NULL;
+
+	printf("enter string: ");
+	gets(str);
+
+	int k = 0;
+	while (str[k] != '\0')
+	{
+		tr = toNode(str[k], tr);
+		k++;
+	}
+	printf("enter char: ");
+	char ch;
+	scanf("%c", &ch);
+
+	Show(tr, 0);
+	howNodes = ndCouter(tr, ch, 0, 0);
+	printf("How nodes: %c = %d", ch, howNodes);
+	clTree(tr);
+	
+	return 0;
+}
