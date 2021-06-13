cd0c3b4f704190b5be435b4da1b9b7e9a2b6d51c
Vlados-G <83881225+Vlados-G@users.noreply.github.com>
2021-06-14 02:03:25 +0300

    Горожанин Владислав lab#1 variant-#5

---
+++ Gorozhankin Vladislav/lab№4.1.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CPP_OVERLOAD_STANDARD_FUNC_0_0_CGETS
+#include <stdio.h>
+#include <string.h>
+#include<locale.h>
+#define DELIM " ,.\t\n"
+/*var.5 Разнобуквица. С клавиатуры вводится многострочный текст.
+Распечатать самое длинное слово, в котором все буквы разные.*/
+int unique_letters(const char* s) {
+    return (!*s) ? 1 : strchr(s + 1, *s) ? 0 : unique_letters(s + 1);
+}
+
+int main(void) {
+    setlocale(LC_ALL, "Russian");
+    char text[1024];
+    printf("Введите текст: \n");
+    fgets(text, sizeof(text) - 1, stdin);
+    char* ptr;
+    const char* longestUnique = "";
+
+    for (ptr = strtok(text, DELIM); ptr; ptr = strtok(NULL, DELIM))
+        if (unique_letters(ptr) && strlen(ptr) > strlen(longestUnique))
+            longestUnique = ptr;
+
+    printf("%s\n", (*longestUnique) ? longestUnique : "No suitable words found.");
+
+    return 0;
+}
---
+++ Gorozhankin Vladislav/lab№4.2.cpp
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+/* variant 5(Длина строк текстового файла не превышает 80 символов.
+Разработать программу центрирования строк: короткие строки
+дополняются пробелами и размещаются по центру.) */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	FILE* f;
+	char** str;
+	int i, j, number, add;
+	fpos_t pos;
+
+	str = (char**)malloc(100 * sizeof(char*));
+	for (i = 0; i < 100; i++) {
+		str[i] = (char*)malloc(82 * sizeof(char));
+	}
+
+	f = fopen("text.txt", "r+");
+	setbuf(f, NULL);
+	fgetpos(f, &pos);
+
+	i = 0;
+	while (!feof(f)) {
+		fgets(str[i], 81, f);
+		i++;
+	}
+	number = i;
+
+	rewind(f);
+
+	for (i = 0; i < number - 1; i++) {
+		add = strlen(str[i]) - 1;
+		add = (80 - add) / 2;
+		for (j = 0; j < add; j++) {
+			fputc(' ', f);
+		}
+		fputs(str[i], f);
+	}
+
+	add = strlen(str[i]);
+	add = (80 - add) / 2;
+	for (j = 0; j < add; j++) {
+		fputc(' ', f);
+	}
+	fputs(str[i], f);
+
+	//-----------------------------------
+	for (i = 0; i < 100; i++) {
+		free(str[i]);
+	}
+	free(str);
+	fclose(f);
+
+	getchar();
+	return 0;
+}
