8552e43e3d62b492035f19e480e9d7127e101ed8
Vlados-G <83881225+Vlados-G@users.noreply.github.com>
2021-06-14 02:15:15 +0300

    Горожанин Владислав lab#5 variant-#4

---
+++ Gorozhankin Vladislav/lab№5.cpp
@@ -0,0 +1,132 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+#include <math.h>
+#include <string.h>
+/*var.4(Разработать функцию, переводящую многоразрядное десятичное
+число в восьмеричную систему. Для хранения многоразрядного
+числа использовать динамический двунаправленный список.)*/
+
+typedef struct Node {
+	int digit;
+	struct Node* next;
+	struct Node* prev;
+} Node;
+typedef struct List {
+	int n;
+	Node* head;
+	Node* tail;
+} List;
+
+List CreateNumber(char* a);
+void AddDigit(List* num, int d);
+List DecToOct(List* num);
+int GetNumber(List* num);
+void RemoveBack(List* num);
+void print(List* num);
+void printInvert(List* num);
+void freeList(List* num);
+
+int main(void) {
+	long long a;
+	char str[21];
+	printf("Enter number: ");
+	if (!scanf("%20[0-9]", &str)) 
+	{
+		printf("Incorrect input"); 
+		return 1; 
+	}
+	List num = CreateNumber(str);
+	print(&num);
+	List oct = DecToOct(&num);
+	printInvert(&oct);
+
+	freeList(&oct);
+
+	return 0;
+}
+List CreateNumber(char* a) {
+	List num = { 0, NULL, NULL };
+
+	for (int i = 0; i < strlen(a); i++) {
+		AddDigit(&num, a[i] - '0');
+	}
+	return num;
+}
+void AddDigit(List* num, int d) {
+	Node* p = (Node*)malloc(sizeof(Node));
+	p->digit = d;
+	p->next = NULL;
+	p->prev = NULL;
+	if (!num->head) { num->head = p; num->tail = p; }
+	else {
+		num->tail->next = p;
+		p->prev = num->tail;
+		num->tail = p;
+	}
+	num->n++;
+}
+
+List DecToOct(List* num) {
+	int dec = 0, oct = 0, i = 0, ost = 0;
+	List octnumInvert = { 0, NULL, NULL };
+	do {
+		dec = GetNumber(num);
+		if (!num->n) { num->head = NULL; num->tail = NULL; }
+		//print(num);
+		ost = dec % 8;
+		dec /= 8;
+		char buf[21];
+		snprintf(buf, 21, "%d", dec);
+		*num = CreateNumber(buf);
+		//print(num);
+		AddDigit(&octnumInvert, ost);
+		//printInvert(&octnumInvert);
+	} while (dec);
+	return octnumInvert;
+}
+
+int GetNumber(List* num) {
+	int dec = 0, i = 0;
+	while (num->tail) {
+		dec += num->tail->digit * pow(10, i++);
+		RemoveBack(num);
+	}
+	return dec;
+}
+void RemoveBack(List* num) {
+	Node* tmp = num->tail;
+	num->tail = num->tail->prev;
+	free(tmp);
+	num->n--;
+}
+
+void print(List* num) {
+	printf("Number in dec: ");
+	Node* p = num->head;
+	while (p) {
+		printf("%d", p->digit);
+		p = p->next;
+	}
+	printf("\n");
+}
+void printInvert(List* num) {
+	Node* p = num->tail;
+	printf("Number in oct: ");
+	while (p) {
+		printf("%d", p->digit);
+		p = p->prev;
+	}
+	printf("\n");
+}
+
+void freeList(List* num) {
+	Node* tmp;
+	while (num->head) {
+		tmp = num->head;
+		num->head = num->head->next;
+		free(tmp);
+		num->n--;
+	}
+}
